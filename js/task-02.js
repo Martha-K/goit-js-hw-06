15f693021412fbbb34dfdc5fdb0a7a972a9e296d
Martha-K <kozenyukmarta@gmail.com>
2023-02-26 02:11:34 +0200

    the second task is completed

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,16 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+ingredients.forEach((el) => {
+  const liElement = document.createElement("li");
+  liElement.appendChild(document.createTextNode(el));
+  liElement.classList.add("item");
+  ingredientsList.appendChild(liElement);
+});
