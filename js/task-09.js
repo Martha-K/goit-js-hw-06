12fa56eb2c05cf5752ee276108b3ebab02a4ac82
Martha-K <kozenyukmarta@gmail.com>
2023-03-06 01:11:14 +0200

    completed all tasks

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const refs = {
+  pageBackground: document.querySelector(".widget"),
+  nameColor: document.querySelector(".color"),
+  colorChangeButton: document.querySelector(".change-color"),
+};
+
+refs.colorChangeButton.addEventListener("click", changeBackgroundColor);
+
+function changeBackgroundColor() {
+  const backgroundColor = getRandomHexColor();
+  refs.pageBackground.style.backgroundColor = backgroundColor;
+  refs.nameColor.textContent = backgroundColor;
+}
